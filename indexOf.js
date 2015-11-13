3546986e23f37cf4299fe26751b7923d9c9dab06
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-13 17:31:53 +0100

    Update indexOf.js

--- indexOf.js
+++ indexOf.js
@@ -1,7 +1,7 @@
 var AP=Array.prototype;
 AP.indexOf=function(searchElement,fromIndex){
 	var i=fromIndex,o=this,x=o.length,l=x>>>0;
-	if(l!==0 && (i=isNaN(i)?0:parseInt(i),i<l)){
+	if(l!==0 && (i=(i===Infinity||isNaN(i))?0:parseInt(i),i<l)){
 		x=Math;
 		i=x.max(i>=0?i:l-x.abs(i),0);
 		x=searchElement;
